0a61effc1f8978395212ae139bd7cbbb3e75a6f9
jeanhern81 <jeanhern81@gmail.com>
2021-04-14 17:40:24 -0700

    fixing broken links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,14 +17,14 @@ class NavBarMain extends Component {
     
     <Router>
                 <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="/">Kulcher Tek Podcast</Navbar.Brand>
+    <Navbar.Brand href="/home">Kulcher Tek Podcast</Navbar.Brand>
     <Nav className="mr-auto">
 
-        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/home">Home</Nav.Link>
         <Nav.Link href="/crew">Meet the Crew</Nav.Link>
 
-        <Nav.Link href="/https://kulchertek.podbean.com/" target='_blank'>Podcast</Nav.Link>
-        <Nav.Link href="/https://www.patreon.com/kulchertek?fan_landing=true" target='_blank'>Patreon</Nav.Link>
+        <Nav.Link href="https://kulchertek.podbean.com/" target='_blank'>Podcast</Nav.Link>
+        <Nav.Link href="https://www.patreon.com/kulchertek?fan_landing=true" target='_blank'>Patreon</Nav.Link>
 
         <NavDropdown title="Shop Merch" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Clothing</NavDropdown.Item>
