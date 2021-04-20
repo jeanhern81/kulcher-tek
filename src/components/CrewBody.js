75faa9c20b8f1a4672e63f696db68dd798b06634
jeanhern81 <jeanhern81@gmail.com>
2021-04-20 10:44:26 -0700

    revisions

--- src/components/CrewBody.js
+++ src/components/CrewBody.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import { Card } from "react-bootstrap";
 
 import Me from '../images/Me.png';
+import gerson from '../images/gerson.png';
 //Style Sheet
 import '../index.css';
 
@@ -18,14 +19,14 @@ return (
 <Card style={{ }} >
 <Card.Body >
         <h3 >Gerson</h3> 
+        <br />
         <Col xs={6} md={4} ></Col>
-        
+        <img src={gerson} style={{ width: 175, float: 'left'}} id='gerson' alt='Gerson Marroquin' ></img>
 
-        <div className='gmTxt'>
+        <div >
         <Col>   
-        <p >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p className='gmTxt'>
+        Technology has been a part of me for as long as I can remember. From playing with my cousin's Atari to understanding how a VCR operates and connecting it to multiple components. This would eventually have computers and coding cross my path. From having a Compaq desktop to building my own and playing with MySpace to developing a full blown working web application, technology has been a big part of my life. With all of my experiences, I hope to share some of my knowledge to those who might not be sure if technology is their future or are on the fence of just picking it up for fun. Trust me. It's worth it. With all that said however, my true passion will always be comic books. Specifically Teenage Mutant Ninja Turtles. Let's see how I combine my passion with my love. 
         </p>
         </Col>
     
@@ -37,16 +38,17 @@ return (
     </Card.Body>
 
     <Card.Body >
-        <h3 >Jeanette</h3>  
+        <h3 >Jeanette</h3> 
+        <br /> 
         <Col xs={6} md={4} >
         <img src={Me} style={{width: 175, float:'left' }} id='me' alt='Jeanette Hernandez' ></img>
             
         </Col> 
         
 
-        <div className='jeanetteTxt'>
+        <div >
         <Col> 
-        <p >
+        <p className='jeanetteTxt'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
